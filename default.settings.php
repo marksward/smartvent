4f167de94299f9d27acf50f12f2f8a6b5c1d24fe
marksward <mark@thewardconnection.com>
2015-07-05 17:50:27 +1200

    Added example default.settings.php file

---
+++ default.settings.php
@@ -0,0 +1,8 @@
+<?php
+    // add correct values for install and renname this file to settings.php
+    // Database settings
+    $dbhost = 'localhost';
+    $dbname = 'smartvent';
+    $dbuser = 'enterusername';
+    $dbpass = 'enteruserdatabasepassword';
+?>
